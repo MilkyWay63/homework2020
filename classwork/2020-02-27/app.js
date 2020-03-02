3b9f245aac86a6f7073fbfca89107cd93919514a
Emilija <secretlyemi@gmail.com>
2020-03-02 18:19:59 +0200

    third commit

---
+++ classwork/2020-02-27/app.js
@@ -0,0 +1,15 @@
+let username = prompt('Username:'),
+    password = prompt('Password:');
+console.log({username, password});
+if (!username || !password) {
+    document.write('Viso gero!');
+} else {
+    if (
+        (username == 'admin' && password == 'master')
+        ||
+        (username && password == username + '11')
+    ) {
+        document.write(`Sveiki, ${username}`);
+    } else  {
+        document.write('Neteisingas slaptažodis');
+    }
